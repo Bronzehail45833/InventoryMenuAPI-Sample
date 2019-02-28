ebe488ba484279205bc1b4155707f3daa9f24c93
korado531m7 <support.system@icloud.com>
2019-02-28 16:39:35 +0900

    For version 2.0.0 API

--- src/korado531m7/InventoryMenuTest/InventoryMenuTest.php
+++ src/korado531m7/InventoryMenuTest/InventoryMenuTest.php
@@ -1,8 +1,11 @@
 <?php
 namespace korado531m7\InventoryMenuTest;
 
-use korado531m7\InventoryMenuAPI\InventoryMenuAPI as IM;
-use korado531m7\InventoryMenuAPI\event\InventoryMenuClickEvent;
+use korado531m7\InventoryMenuAPI\InventoryMenuAPI;
+use korado531m7\InventoryMenuAPI\InventoryMenu;
+use korado531m7\InventoryMenuAPI\InventoryTypes;
+use korado531m7\InventoryMenuAPI\event\InventoryClickEvent;
+
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerInteractEvent;
@@ -11,28 +14,38 @@ use pocketmine\item\Item;
 class InventoryMenuTest extends PluginBase implements Listener{
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        InventoryMenuAPI::register($this);
     }
     
     public function onJoin(PlayerInteractEvent $event){
         $p = $event->getPlayer();
         if($p->isOp()){
             $item = [13 => Item::get(116)->setCustomName('§6INVENTORY MENU TESTING')->setLore(['','§aYou can use inventory menu api like this','§6It\'s awesome']), 38 => Item::get(276)->setCustomName('§6Calculation Test'), 42 => Item::get(331)->setCustomName('§aWhich do you like?')];
-            IM::sendInventoryMenu($p, $item, '§bExample InventoryMenu', IM::INVENTORY_TYPE_DOUBLE_CHEST);
+            $menu = new InventoryMenu(InventoryTypes::INVENTORY_TYPE_DOUBLE_CHEST);
+            $menu->setContents($item);
+            $menu->setName('§bExample InventoryMenu');
+            $menu->send($p);
         }
     }
     
-    public function onClicked(InventoryMenuClickEvent $event){
+    public function onClicked(InventoryClickEvent $event){
         $p = $event->getPlayer();
-        $name = $event->getMenuName();
+        $inv = $event->getInventory();
         $item = $event->getItem();
-        switch($name){
+        switch($inv->getName()){
             case '§bExample InventoryMenu':
                 if($item->getId() === 276){
                     $items = [0 => Item::get(332)->setCustomName('§l§b3'), 4 => Item::get(332)->setCustomName('§l§b6')];
-                    IM::fillInventoryMenu($p, $items, '§6What is 1 + 5?', IM::INVENTORY_TYPE_HOPPER);
+                    $menu = new InventoryMenu(InventoryTypes::INVENTORY_TYPE_HOPPER);
+                    $menu->setContents($items);
+                    $menu->setName('§6What is 1 + 5?');
+                    $menu->send($p);
                 }else{
                     $items = [Item::get(260),Item::get(282),Item::get(297),Item::get(320),Item::get(349),Item::get(354),Item::get(357),Item::get(360),Item::get(364)];
-                    IM::fillInventoryMenu($p, $items, '§aWhich do you like?', IM::INVENTORY_TYPE_DISPENSER);
+                    $menu = new InventoryMenu(InventoryTypes::INVENTORY_TYPE_DISPENSER);
+                    $menu->setContents($items);
+                    $menu->setName('§aWhich do you like?');
+                    $menu->send($p);
                 }
             break;
             
